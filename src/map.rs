ed1723bee1d4a5e70631adabc1f6da4dc26f14d7
Xoillaz <xorillaz.persona@gmail.com>
2024-05-09 15:21:17 +0800

    feat(map): Implement basic tile type and render function

---
+++ src/map.rs
@@ -0,0 +1,37 @@
+use crate::prelude::*;
+const NUM_TILES: usize = (SCREEN_HEIGHT * SCREEN_WIDTH) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn map_idx(x: i32, y: i32) -> usize {
+        // X-first encoding.
+        ((y * SCREEN_WIDTH) + x) as usize
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = Self::map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.')),
+                    TileType::Wall => ctx.set(x, y, GREEN, BLACK, to_cp437('#')),
+                };
+            }
+        }
+    }
+}
