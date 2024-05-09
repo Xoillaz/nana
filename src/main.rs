70e72c98dbcf8d95870b1c0bf86c76393f223e8b
Xoillaz <xorillaz.persona@gmail.com>
2024-05-09 20:59:53 +0800

    feat(camera): Implement graphical dungeon using programmer art

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod map;
 mod map_builder;
 mod player;
@@ -6,7 +7,10 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 4;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 4;
     // Modules are organized as a tree, crate:: means accesses the root.
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -17,6 +21,7 @@ use prelude::*;
 struct State {
     map: Map,
     player: Player,
+    camera: Camera,
 }
 
 impl State {
@@ -27,24 +32,33 @@ impl State {
         Self {
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new() // Create a generic terminal and specify attributes directly.
         .with_title("nana")
-        // Track game speed, tells OS it can rest between frames.
-        .with_fps_cap(30.0)
+        .with_fps_cap(30.0) // Track game speed, tells OS it can rest between frames.
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // The size of subsequent consoles.
+        .with_tile_dimensions(32, 32) // The size of each character in the font file.
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // Add a console using attributes specified.
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // Add a second console to show transparency.
         .build()?;
 
     main_loop(context, State::new())
